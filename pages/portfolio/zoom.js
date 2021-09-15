5a56229308a7326d2c1d97b63238cad40b73bc8f
Mahmoud Hassan <mahmoudhassan@Mahmouds-MacBook-Air.local>
2021-09-16 00:09:49 +0200

    fix projects

--- pages/portfolio/zoom.js
+++ pages/portfolio/zoom.js
@@ -1,10 +1,11 @@
-export default  function (zoomIn, setZoomIn,img ){
+export default function (zoomIn, setZoomIn, img) {
+  if (img) {
     if (zoomIn) {
-        img.style.height = '700px';
-        setZoomIn(!zoomIn);
+      img.style.height = "700px";
+      setZoomIn(!zoomIn);
     } else {
-        img.style.height = '450px';
-        setZoomIn(!zoomIn);
-
+      img.style.height = "450px";
+      setZoomIn(!zoomIn);
     }
+  }
 }
