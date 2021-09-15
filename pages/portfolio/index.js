a2f4e0331a01ab7d134e8c1f6eae95691ab61a91
Mahmoud Hassan <mahmoudhassan@Mahmouds-MacBook-Air.local>
2021-09-16 00:06:37 +0200

    fix projects

--- pages/portfolio/index.js
+++ pages/portfolio/index.js
@@ -3,18 +3,15 @@ import Link from "next/link";
 import Layout from "../../components/layout/Layout";
 import { AiOutlineFolderView } from "react-icons/ai";
 import { motion } from "framer-motion";
-import {projects} from "./projects";
 
 function Portfolio() {
-  const projectItemm = function (){
-    projects.map(function (project,i){
-       <li>
-        {project.name}
-      </li>
-    })
-  }
+  const projectItemm = function () {
+    projects.map(function (project, i) {
+      <li>{project.name}</li>;
+    });
+  };
   return (
-    <Layout >
+    <Layout>
       <motion.div
         className={styles.container}
         initial={{ opacity: 0 }}
@@ -35,58 +32,63 @@ function Portfolio() {
                 join your team? Contact me. I will answer you in ~24 hours.
               </div>
               <ul className={styles.projects}>
-                {!projects ?
-                    <div className='pre_loader'>
-                      <img src="../loader.gif" alt=""/>
-                    </div>
-                    : projects.map(function (project,i){
-                     return <li key={i}>
-                       <div className={styles.project_item}>
-                         <div className={styles.grid_2_50}>
-                           <div className={styles.projrct_details}>
-                             <div className={styles.h1}>{project.name}</div>
-                             <div className={styles.p}>
-                               {project.shortDesc}
-                             </div>
-                             <div className={styles.p_bold}>
-                               {project.desc}
-                             </div>
-                             <ul className={styles.project_desc}>
-                               {project.futures.map(function (f,i){
-                                 return <li key={i}>{f}</li>
-                               })}
-                             </ul>
-                             <Link href={`/portfolio/${project.name}`}>
-                               <div className={styles.more}>
-                                 <button>
-                                   <div className={styles.icon}>
-                                     <AiOutlineFolderView />
-                                   </div>
-                                   More details
-                                 </button>
-                               </div>
-                             </Link>
-                             <div className={styles.tecno}>
-                               <div className={styles.tecno_head}>TECHNOLOGIES</div>
-                             </div>
-                             <ul className={styles.tecno_body}>
-                               {project.techno.map(function (t,i){
-                                 return <li key={i}>{t}</li>
-                               })}
-                             </ul>
-                           </div>
-                           <Link href={`/portfolio/${project.name}`}>
-                             <div className={styles.project_img}>
-                               <div className={styles.img}>
-                                 <img src={project.img} alt="yalpcamp" />
-                               </div>
-                             </div>
-                           </Link>
-                         </div>
-                       </div>
-                     </li>
-                    })
-                }
+                {!projects ? (
+                  <div className="pre_loader">
+                    <img src="../loader.gif" alt="" />
+                  </div>
+                ) : (
+                  projects.map(function (project, i) {
+                    return (
+                      <li key={i}>
+                        <div className={styles.project_item}>
+                          <div className={styles.grid_2_50}>
+                            <div className={styles.projrct_details}>
+                              <div className={styles.h1}>{project.name}</div>
+                              <div className={styles.p}>
+                                {project.shortDesc}
+                              </div>
+                              <div className={styles.p_bold}>
+                                {project.desc}
+                              </div>
+                              <ul className={styles.project_desc}>
+                                {project.futures.map(function (f, i) {
+                                  return <li key={i}>{f}</li>;
+                                })}
+                              </ul>
+                              <Link href={`/portfolio/${project.name}`}>
+                                <div className={styles.more}>
+                                  <button>
+                                    <div className={styles.icon}>
+                                      <AiOutlineFolderView />
+                                    </div>
+                                    More details
+                                  </button>
+                                </div>
+                              </Link>
+                              <div className={styles.tecno}>
+                                <div className={styles.tecno_head}>
+                                  TECHNOLOGIES
+                                </div>
+                              </div>
+                              <ul className={styles.tecno_body}>
+                                {project.techno.map(function (t, i) {
+                                  return <li key={i}>{t}</li>;
+                                })}
+                              </ul>
+                            </div>
+                            <Link href={`/portfolio/${project.name}`}>
+                              <div className={styles.project_img}>
+                                <div className={styles.img}>
+                                  <img src={project.img} alt="yalpcamp" />
+                                </div>
+                              </div>
+                            </Link>
+                          </div>
+                        </div>
+                      </li>
+                    );
+                  })
+                )}
               </ul>
             </div>
           </div>
